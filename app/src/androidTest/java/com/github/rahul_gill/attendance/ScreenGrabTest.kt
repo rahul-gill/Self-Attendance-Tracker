a0def62b44baccb97770c5f2a69d5825c6088858
Rahul Gill <rgill1@protonmail.com>
2026-03-24 14:19:18 +0530

    added onboarding screen to faslane screenshots

--- app/src/androidTest/java/com/github/rahul_gill/attendance/ScreenGrabTest.kt
+++ app/src/androidTest/java/com/github/rahul_gill/attendance/ScreenGrabTest.kt
@@ -1,6 +1,5 @@
 package com.github.rahul_gill.attendance
 
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -8,25 +7,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performKeyInput
 import androidx.compose.ui.test.pressKey
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.rahul_gill.attendance.db.AttendanceCounts
-import com.github.rahul_gill.attendance.db.AttendanceRecordHybrid
 import com.github.rahul_gill.attendance.db.ClassDetail
 import com.github.rahul_gill.attendance.db.CourseClassStatus
 import com.github.rahul_gill.attendance.db.CourseDetailsOverallItem
 import com.github.rahul_gill.attendance.db.DBOps
 import com.github.rahul_gill.attendance.db.ExtraClassTimings
 import com.github.rahul_gill.attendance.prefs.PreferenceManager
-import com.github.rahul_gill.attendance.prefs.UnsetClassesBehavior
 import com.github.rahul_gill.attendance.ui.RootNavHost
 import com.github.rahul_gill.attendance.ui.comps.AttendanceAppTheme
 import com.github.rahul_gill.attendance.ui.comps.ColorSchemeType
@@ -59,6 +54,7 @@ class ScreenGrabTest {
     @Before
     fun init() {
         val dbOps = DBOps.instance
+        PreferenceManager.onboardingCompleted.setValue(true)
         Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
         courses.forEach {
             dbOps.createCourse(
@@ -114,9 +110,29 @@ class ScreenGrabTest {
         }
     }
 
+    @Test
+    fun onboardingScreenshots() {
+        PreferenceManager.onboardingCompleted.setValue(false)
+        composeTestRule.setContent {
+            AttendanceAppTheme(
+                colorSchemeType = ColorSchemeType.WithSeed(Color.Green)
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RootNavHost()
+                }
+            }
+        }
+        composeTestRule.takeScreenshot("0_onboarding")
+    }
+
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun mainScreenScreenshots() {
+        PreferenceManager.onboardingCompleted.setValue(true)
         composeTestRule.setContent {
             AttendanceAppTheme(
                 colorSchemeType = ColorSchemeType.WithSeed(Color.Green)
@@ -132,6 +148,9 @@ class ScreenGrabTest {
             }
         }
         //main screen
+        composeTestRule.waitUntil(10000) {
+            composeTestRule.onAllNodesWithTag("courses_button").fetchSemanticsNodes().isNotEmpty()
+        }
         composeTestRule.takeScreenshot("1_main_screen_today")
         composeTestRule.onNodeWithTag(testTag = "courses_button").performClick()
         composeTestRule.takeScreenshot("2_main_screen_overall_courses")
@@ -173,78 +192,10 @@ class ScreenGrabTest {
             courseName = name,
             requiredAttendance = 75.0,
             currentAttendancePercentage = 100.0 * presents / (presents + absents),
-            presents = presents.toInt(),
-            absents = absents.toInt(),
-            cancels = cancels.toInt()
+            presents = presents,
+            absents = absents,
+            cancels = cancels
         )
-    }
-    private val demoTodayClasses: List<Pair<AttendanceRecordHybrid, AttendanceCounts>>
-        get() {
-            val date = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
-            var startTime = LocalTime.of(10, 0)
-            val endTime = { startTime.plusHours(1) }
-            val list = mutableListOf<AttendanceRecordHybrid>()
-
-            for (i in 0..7) {
-                if (i == 5) {
-                    AttendanceRecordHybrid.ExtraClass(
-                        extraClassId = i.toLong(),
-                        startTime = startTime,
-                        endTime = endTime(),
-                        courseName = courses[i].courseName,
-                        date = date,
-                        classStatus = CourseClassStatus.Present,
-                        courseId = i.toLong()
-                    )
-                } else {
-                    list.add(
-                        AttendanceRecordHybrid.ScheduledClass(
-                            attendanceId = i.toLong(),
-                            scheduleId = i.toLong(),
-                            startTime = startTime,
-                            endTime = endTime(),
-                            courseName = courses[i].courseName,
-                            date = date,
-                            classStatus = if (i == 2) CourseClassStatus.Absent else CourseClassStatus.Present,
-                            courseId = i.toLong()
-                        )
-                    )
-                }
-                startTime = startTime.plusHours(1)
-            }
-
-
-            return list.map { record ->
-                Pair(
-                    record,
-                    courses.find { it.courseId == record.courseId }!!.run {
-                        val presentsLater =
-                            (presents + if (PreferenceManager.unsetClassesBehavior.value == UnsetClassesBehavior.ConsiderPresent)
-                                unsets else 0).toLong()
-                        val absentsLater =
-                            (absents + if (PreferenceManager.unsetClassesBehavior.value == UnsetClassesBehavior.ConsiderAbsent)
-                                unsets else 0).toLong()
-                        AttendanceCounts(
-                            if (absentsLater + presentsLater == 0L) 100.0 else 100.0 * presentsLater / (absentsLater + presentsLater),
-                            presentsLater,
-                            absentsLater,
-                            cancels.toLong(),
-                            unsets.toLong(),
-                            requiredAttendance
-                        )
-                    }
-                )
-            }
-        }
-
-    private fun <T : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<T>, T>.takeScreenshot(
-        screenshotName: String
-    ) {
-        runBlocking {
-            awaitIdle()
-            delay(100)
-            Screengrab.screenshot(screenshotName)
-        }
     }
 
     private fun ComposeContentTestRule.takeScreenshot(
